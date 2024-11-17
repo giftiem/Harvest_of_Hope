0ad79f2a0294de6a13a770621f15f3a80f2b33d5
Calistas Mlilo <cmlilo2004@gmail.com>
2024-11-17 13:30:39 +0200

    Update run.py

--- run.py
+++ run.py
@@ -241,5 +241,5 @@ def load_data():
     
 if __name__ == '__main__':
     # fetch_projects("ZA", "207d0f21-65f0-4a5d-8084-5d972d341309")
-    app.run(host="0.0.0.0", port=5000,debug=True)
+    app.run(host="0.0.0.0", port=5000)
 
